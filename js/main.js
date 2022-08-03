5d891e84c0e69baffc3b6b9380693b7e435b1443
daniela ventura <danielavensec@gmail.com>
2022-08-03 16:46:52 -0700

    edited 8 ball images

--- js/main.js
+++ js/main.js
@@ -1,95 +1,51 @@
-
-
-function ask() 
-{
+function ask() {
      var magicImages = [{
         src: "magic/magic8ball_1.png",
-        width: "500",
-        height: "500"
+        
     }, {
         src: "magic/magic8ball_2.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_3.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_4.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_5.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_6.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_7.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_8.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_9.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_10.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_11.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_12.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_13.png",
-        width: "500",
-        height: "500"
     }, {
         src: "magic/magic8ball_14.png",
-        width: "300",
-        height: "500"
     }, {
         src: "magic/magic8ball_15.png",
-        width: "300",
-        height: "500"
     }, {
         src: "magic/magic8ball_16.png",
-        width: "300",
-        height: "500"
     }, {
         src: "magic/magic8ball_17.png",
-        width: "300",
-        height: "500"
     }, {
         src: "magic/magic8ball_18.png",
-        width: "300",
-        height: "500"
     }, {
         src: "magic/magic8ball_19.png",
-        width: "300",
-        height: "500"
     }, {
         src: "magic/magic8ball_20.png",
-        width: "300",
-        height: "500"
     }];
     
     var randomNumber = [];
     for (var i = 0, j = magicImages.length; i < j; i++) {
         randomNumber[i] = new Image();
         randomNumber[i].src = magicImages[i].src;
-        randomNumber[i].width = magicImages[i].width;
-        randomNumber[i].height = magicImages[i].height;
     }
    
 // create random image number
@@ -115,43 +71,7 @@ for (var p = 0; p < l; p++) {
 document.body.appendChild(newImage);
 }
 
-/*
 
-     magicImages = [
-        'magic/magic8ball_1.png',
-        'magic/magic8ball_2.png',
-        'magic/magic8ball_3.png',
-        'magic/magic8ball_4.png',
-        'magic/magic8ball_5.png',
-        'magic/magic8ball_6.png',
-        'magic/magic8ball_7.png',
-        'magic/magic8ball_8.png',
-        'magic/magic8ball_9.png',
-        'magic/magic8ball_10.png',
-        'magic/magic8ball_11.png',
-        'magic/magic8ball_12.png',
-        'magic/magic8ball_13.png',
-        'magic/magic8ball_14.png',
-        'magic/magic8ball_15.png',
-        'magic/magic8ball_16.png',
-        'magic/magic8ball_17.png',
-        'magic/magic8ball_18.png',
-        'magic/magic8ball_19.png',
-        'magic/magic8ball_20.png',
-        ];
-    
-
- function ask() 
-{ 
-    
-        randomResponse = Math.floor(Math.random() * magicImages.length);
-            
-        
-        selectedResponse = magicImages [randomResponse]
-    
-         document.getElementById ('answers').src = `./magic/${selectedResponse}`
-     
-}
-
-*/
-
+function refresh() {
+    window.location.reload();
+};
